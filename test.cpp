7a295d1898ac907e8905c4ba20577c7a5eb9059f
Carry <a2294510419@163.com>
2021-09-27 09:51:56 +0800

    每日一道简单题

--- test.cpp
+++ test.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
-    int main(){
-        cout<<"I'm Xia"<<endl;
+    class test{
+        public:
+            test(int n): num1(n) { }
+            test operator = (const test & t){
+                num1 = t.num1;
+                return *this;
+            }
+            void display(){
+                cout<< num1<<endl;
+            }
+        private:
+        int num1;
+    };
+    
+int main(){
+        test t1(3);
+        t1.display();
+        test t2(4);
+        t2.display();
+        t2 = t1;
+        t2.display();
+        return 1;
     }
     
