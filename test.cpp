d5f28c224804c10bd5b7df7712b3255a814c6981
Carry <a2294510419@163.com>
2021-10-09 15:20:05 +0800

    成功写出的第一道middle题--2021.10.9

--- test.cpp
+++ test.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 class Solution {
 public:
-    bool IsReverse(string s, int length){ //判断字符串是否是回文串
-        {
-            for(int i = 0; i<length/2; i++)
-                if(s[i]!=s[length-1-i])
-                    return false;
-            return true;
+    string convert(string s, int numRows) {
+
+        if (numRows == 1) return s;
+
+        vector<string> rows(min(numRows, int(s.size())));
+        int curRow = 0;
+        bool goingDown = false;
+
+        for (char c : s) {
+            rows[curRow] += c;
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+            curRow += goingDown ? 1 : -1;
         }
-    }
-    string longestPalindrome(string s) {
-        for(int i = s.size(); i>1; i--){ //判断字符串s的所有长度为i的子串中是否存在回文串
-            for(int j = 0; j+i<=s.size(); j++)
-            {
-                if(IsReverse(s.substr(j,i), i)) //如果是回文串，那么返回这个子串
-                    return s.substr(j,i);
-            }
-        }
-        return s.substr(0,1);
+
+        string ret;
+        for (string row : rows) ret += row;
+        return ret;
     }
 };
-    
+  
 int main(){
-        string s = "abbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb";
+        string s = "PAYPALISHIRING";
+        int numRows = 4;
         Solution sol;
-        cout<<sol.longestPalindrome(s);
+        cout<<sol.convert(s, numRows);
     }
     
