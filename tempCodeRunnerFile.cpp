0e12363ff3274f4bea5d66ef4145ab15c0f80ebc
Carry <a2294510419@163.com>
2021-10-11 23:41:27 +0800

    1

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,1 @@
-#include <iostream>
+m1
--- test.cpp
+++ test.cpp
@@ -1,31 +1,22 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 using namespace std;
-class Solution {
-public:
-    string convert(string s, int numRows) {
-
-        if (numRows == 1) return s;
-
-        string ret;
-        int n = s.size();
-        int cycleLen = 2 * numRows - 2;
-
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j + i < n; j += cycleLen) {
-                ret += s[j + i];
-                if (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
-                    ret += s[j + cycleLen - i];
-            }
-        }
-        return ret;
+class Person {
+    public:
+      virtual ~Person(){
+        cout<<"I'm father"<<endl;
     }
 };
 
+class Man:  public Person{
+    public:
+        ~Man(){
+        cout<<"I'm son"<<endl;
+    }
+};
 int main(){
-        string s = "PAYPALISHIRING";
-        int numRows = 4;
-        Solution sol;
-        cout<<sol.convert(s, numRows);
+        Person* m1 = new Man;
+        delete m1;
     }
     
