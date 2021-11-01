94e870a85a974eb55e3583faccc187cd34a822e2
Carry <a2294510419@163.com>
2021-11-01 09:16:13 +0800

    AC了middle题整数转罗马数字

--- test.cpp
+++ test.cpp
@@ -1,22 +1,47 @@
 #include <iostream>
 #include <vector>
-#include <climits>
+#include <map>
+#include <string>
+#include<unordered_set>
 using namespace std;
-class Person {
-    public:
-      virtual ~Person(){
-        cout<<"I'm father"<<endl;
-    }
-};
-
-class Man:  public Person{
-    public:
-        ~Man(){
-        cout<<"I'm son"<<endl;
+class Solution {
+public:
+    string intToRoman(int num) {
+        string result;        
+        map<int, string, greater<int>> m1 =
+        {   
+            {1000, "M"},
+            {900, "CM"},
+            {500, "D"},
+            {400, "CD"}, 
+            {100, "C"},
+            {90, "XC"},
+            {50, "L"},
+            {40,"XL"},
+            {10, "X"},
+            {9,"IX"},
+            {5, "V"},
+            {4,"IV"},
+            {1, "I"}
+        };
+        while(num>0)
+        {
+            auto ite = m1.begin();
+            for(; ite != m1.end(); ite++)
+            {
+                if(num >= ite->first)
+                {
+                    num-=ite->first;
+                    result+=ite->second;        
+                }
+            }
+        }
+        return result;
     }
 };
 int main(){
-        Person* m1 = new Man;
-        delete m1;
+        int num = 20;
+        Solution sol;
+        cout<<sol.intToRoman(num);
     }
     
