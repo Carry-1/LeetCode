9b788081f44b6b0f56c165bf1cd90da7345bf22f
Carry <a2294510419@163.com>
2022-03-16 22:53:58 +0800

    modify

--- test.cpp
+++ test.cpp
@@ -1,27 +1,10 @@
-#include<string>
 #include<iostream>
 using namespace std;
-class Solution {
-public:
-    string replaceSpace(string s) {
-    string delim = "%20";
-    for(int i=0; i<s.size(); i++)
-    {
-        if(s[i]==' ')
-        {
-            string pre = s.substr(0,i);
-            string post = s.substr(i+1,s.size()-i-1);
-            s = pre + delim + post;
-            i += delim.size();
-        }
-    }
-    return s;
-    }
-};
 int main()
 {
-    string s = "  ";
-    Solution sol;
-    cout<<sol.replaceSpace(s);
+    int i = 4;
+    int && r1 = std::move(i);
+    cout<<r1<<endl;
+    cout<<i<<endl;
     return 0;
 }
